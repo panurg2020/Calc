d6a7511eec79620a4e7300af9f580fc3aa377b74
panurg2020 <59584452+panurg2020@users.noreply.github.com>
2022-05-02 16:28:48 +0300

    Add files via upload

--- Calc.java
+++ Calc.java
@@ -12,10 +12,10 @@ public class Calc {
 
     public static void main (String[] args) {
         System.out.println("Введите арифметический пример - арабские, либо римские цифры, формат - одна строка с двумя числами и одним знаком операции, числа от 0-9 арабские и от 1-10 в римские,использовать только целые числа");
-        String vvod = s.nextLine();
-        char[] arr= vvod.toCharArray();
-        if (arr.length <= 5 && arr.length >= 3) {
-            for (int i = 0; i < arr.length; i++) {
+        String vvod = s.nextLine(); //считал ввод
+        char[] arr= vvod.toCharArray(); //ввод перевожу в массив с символами
+        if (arr.length <= 5 && arr.length >= 3) {  //проверка на длину строки, чтобы отсечь неверный ввод
+            for (int i = 0; i < arr.length; i++) {  //переборка на знаки операции, даже если их более одного
                 
                 if (arr[i] == '+') {
                     operation = '+';
@@ -30,52 +30,52 @@ public class Calc {
                     operation = '/';
                 }
             }
-           // String vvodString = String.valueOf(arr);
-            String[] razbiv = vvod.split("[+-/*]");
-            if (razbiv.length<=2){
-            String pervoe = razbiv[0];
-            if (pervoe.length() <= 2){
+            String[] razbiv = vvod.split("[+-/*]"); //разбиваю на числа между знаками
+            if (razbiv.length<=2) {    // проверка на второй знак операции
+            String pervoe = razbiv[0];  
+            if (pervoe.length() <= 2){ // проверка на двузначное число
             String vtoroe = razbiv[1];
-            //String vtoroe = stable01.trim();
-            if (vtoroe.length()<=2) {    		
-            number1 = convert(pervoe);
+            number1 = convert(pervoe); //конвертация из римских
             number2 = convert(vtoroe);
-           
-            if (number1 < 1 && number2 < 1) {
-                result = 0;
-            } else {  
-           
-            result = calculation(number1, number2, operation);
-            String resultRoman = convertRoman(result);
-            System.out.println(pervoe + " " + operation + " " + vtoroe + " = " + resultRoman);
-            
+            if (number1 > 0 && number2 > 0) { //если имеются римские
+                result = calculation(number1, number2, operation);
+                if (result > 0) {  // проверка на отрицательнй результат в римских
+                String resultRoman = convertRoman(result); // конвертация в арабские
+                System.out.println( resultRoman);
+                }
+                else {System.out.println ("Ошибка - в римской системе нет отрицательных");}
             }
+            else if (number1 < 0 && number2 < 0){// если только арабские
             number1 = Integer.parseInt(pervoe);
             number2 = Integer.parseInt(vtoroe);
             result = calculation(number1, number2, operation);
-            System.out.println(number1 + " " + operation + " " + number2 + " = " + result);
-            }         
-            else {
-            	System.out.println ("Только двузначное число");
-            }
-            }
-        else { 
-              System.out.println ("Только двузначное число");
+            System.out.println(result);
         }
+            else if ((number1 < 0 && number2 > 0) || (number1 > 0 && number2 < 0)){
+            System.out.println("Вводите в единой системе - арабской, либо римской ");
+        }
+        
+        else { 
+              System.out.println ("Только двузначные числа");
+        }
+        
+            }
+            else {System.out.println ("Ошибка вода");
+            }
+            }
+            else {System.out.println ("Только один оператор");}
         }
         else {
             System.out.println("Несоответствие заданному вводу, попробуйте ещё раз");
         }
-        }
-        else {System.out.println ("Только один оператор");}
     
     }
-    private static String convertRoman (int num) {
-        String[] roman = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "IXX", "XX"};
+    private static String convertRoman (int num) {// метод возвращает римское из массива
+        String[] roman = {"0","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "IXX", "XX"};
         final String r = roman[num];
         return r;
     }
-    private static int convert (String roman) {
+    private static int convert (String roman) {//метод конвертции в арабские
         try {
             switch (roman) {
                 case "I" -> {
@@ -108,6 +108,36 @@ public class Calc {
                 case "X" -> {
                     return 10;
                 }
+                case "XI" -> {
+                    return 11;
+                }
+                case "XII" -> {
+                    return 12;
+                }
+                case "XIII" -> {
+                    return 13;
+                }
+                case "XIV" -> {
+                    return 14;
+                }
+                case "XV" -> {
+                    return 15;
+                }
+                case "XVI" -> {
+                    return 16;
+                }
+                case "XVII" -> {
+                    return 17;
+                }
+                case "XVIII" -> {
+                    return 18;
+                }
+                case "IXX" -> {
+                    return 19;
+                }
+                case "XX" -> {
+                    return 20;
+                }
                 default ->{
                         }
             }
@@ -116,7 +146,7 @@ public class Calc {
         }
         return -1;
     }
-    public static int calculation (int num1, int num2, char op) {
+    public static int calculation (int num1, int num2, char op) {//выполнение операции возвращает результат
         int res = 0;
         switch (op) {
             case '+' -> res = num1 + num2;
