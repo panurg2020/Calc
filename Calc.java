0087d68b090c50b70bea39c6056bb34002c9fb7c
panurg2020 <59584452+panurg2020@users.noreply.github.com>
2022-05-06 00:02:51 +0300

    Add files via upload
    With Exceptions

--- Calc.java
+++ Calc.java
@@ -10,12 +10,12 @@ public class Calc {
     static char operation;
     static int result;
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws Exception   {
         System.out.println("Введите арифметический пример - арабские, либо римские цифры, формат - одна строка с двумя числами и одним знаком операции, числа от 0-9 арабские и от 1-10 в римские,использовать только целые числа");
         String vvod = s.nextLine(); //считал ввод
         char[] arr= vvod.toCharArray(); //ввод перевожу в массив с символами
-        if (arr.length <= 5 && arr.length >= 3) {  //проверка на длину строки, чтобы отсечь неверный ввод
-            for (int i = 0; i < arr.length; i++) {  //переборка на знаки операции, даже если их более одного
+        if (arr.length <= 5 && arr.length >= 3) {  //проверка на длину строки, чтобы отсечь неверный ввод   
+        for (int i = 0; i < arr.length; i++) {  //переборка на знаки операции, даже если их более одного
                 
                 if (arr[i] == '+') {
                     operation = '+';
@@ -35,43 +35,43 @@ public class Calc {
             String pervoe = razbiv[0];  
             if (pervoe.length() <= 2){ // проверка на двузначное число
             String vtoroe = razbiv[1];
+            try {
             number1 = convert(pervoe); //конвертация из римских
             number2 = convert(vtoroe);
             if (number1 > 0 && number2 > 0) { //если имеются римские
                 result = calculation(number1, number2, operation);
-                if (result > 0) {  // проверка на отрицательнй результат в римских
+                try {
                 String resultRoman = convertRoman(result); // конвертация в арабские
                 System.out.println( resultRoman);
                 }
-                else {System.out.println ("Ошибка - в римской системе нет отрицательных");}
-            }
-            else if (number1 < 0 && number2 < 0){// если только арабские
+                catch (ArrayIndexOutOfBoundsException e) {
+                	throw new Exception("//т.к. в римской системе нет отрицательных чисел");
+                }
+                }
+            else {
             number1 = Integer.parseInt(pervoe);
             number2 = Integer.parseInt(vtoroe);
             result = calculation(number1, number2, operation);
             System.out.println(result);
-        }
-            else if ((number1 < 0 && number2 > 0) || (number1 > 0 && number2 < 0)){
-            System.out.println("Вводите в единой системе - арабской, либо римской ");
-        }
-        
-        else { 
-              System.out.println ("Только двузначные числа");
-        }
-        
             }
-            else {System.out.println ("Ошибка вода");
+           }
+           catch (NumberFormatException e){
+            	throw new Exception("//т.к. используются одновременно разные системы счисления");
             }
             }
-            else {System.out.println ("Только один оператор");}
+            else {throw new Exception("Ошибка ввода- по условию только двузначные числа");
+            }
+            }
+            else {
+            	throw new Exception("//т.к. формат математической операции не удовлетворяет заданию - два операнда и оин оператор (+,-,/,*)") ;
+            	}
+       }
+       else {
+    	   throw new Exception("//т.к. строка не является математической операцией");
         }
-        else {
-            System.out.println("Несоответствие заданному вводу, попробуйте ещё раз");
-        }
-    
     }
     private static String convertRoman (int num) {// метод возвращает римское из массива
-        String[] roman = {"0","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "IXX", "XX"};
+        String[] roman = {"0","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "IXX", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
         final String r = roman[num];
         return r;
     }
@@ -108,36 +108,6 @@ public class Calc {
                 case "X" -> {
                     return 10;
                 }
-                case "XI" -> {
-                    return 11;
-                }
-                case "XII" -> {
-                    return 12;
-                }
-                case "XIII" -> {
-                    return 13;
-                }
-                case "XIV" -> {
-                    return 14;
-                }
-                case "XV" -> {
-                    return 15;
-                }
-                case "XVI" -> {
-                    return 16;
-                }
-                case "XVII" -> {
-                    return 17;
-                }
-                case "XVIII" -> {
-                    return 18;
-                }
-                case "IXX" -> {
-                    return 19;
-                }
-                case "XX" -> {
-                    return 20;
-                }
                 default ->{
                         }
             }
@@ -156,9 +126,7 @@ public class Calc {
                 try {
                     res = num1 / num2;
                 } catch (ArithmeticException | InputMismatchException e) {
-                    System.out.println("Exception : " + e);
                     System.out.println("Нельзя делить на 0");
-
                     break;
                 }
             }
